339b2eafd4234da17a393c35d6b3eb19f39f18fd
Mhamad Ibrahim <skiastep@gmail.com>
2020-11-08 21:16:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+// Code your solutions in this
+const messages = [];
+
+function writeCards(array, keyword) {
+  for (let i = 0; i < array.length; i++) {
+    messages.push(`Thank you, ${array[i]}, for the wonderful ${keyword} gift!`);
+  }
+  return messages;
+}
+
+function countDown(i){
+  while(i>=0){
+    console.log(i--);
+  }
+}
+
+countDown(10);
