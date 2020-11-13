1816672a6cc65b834ad2ea90927ca20f7d9f95d4
Jolie Hobloss <jolieshobloss@gmail.com>
2020-11-13 20:48:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+function writeCards(names,event) {
+  let print=[];
+  for (let i = 0; i < names.length; i++) {   
+  
+     print[i]=`Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+  }
+ 
+  return print;
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+
+function countDown(nb) {  
+  while (nb >= 0) {
+  console.log(nb--);
+}
+  }
+ countDown(10);
+
