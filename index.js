0d4c3c4f677f45a2f05577199c21f3671ff8fcb8
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-11-14 10:12:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let results = [];
+function writeCards(name,event){
+  for(let i=0; i < name.length; i++){
+  
+     results=[...results, `Thank you, ${name[i]}, for the wonderful ${event} gift!`] ;
+  }
+  return results;
+}
+
+
+function countDown(q){
+  while (q>=0){
+    console.log(q--)
+  }
+}
