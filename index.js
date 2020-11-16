e291aaa150048e681783ea1b2f0dd5b9798222cd
layla  hammoud <laylahammoud63@gmail.com>
2020-11-16 12:37:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+  const messages = [];
+  const writeCards = (array, eventName) => {
+  for (let i=0; i<array.length; i++ ) {
+  let message = 'Thank you, ' +  array[i] + ', for the wonderful ' + eventName + ' gift!';
+  messages.push(message);
+  }
+  return messages;
+}
+
+
+const countDown=(x)=>{
+while (x>=0){
+console.log(x--);
+}
+}
+
+
+
+
+//  `Thank you, ${arr[i]} for the wonderful ${event} gift! `;
+//  const myArray = new Array ();
+
+// "Thank you, Ada, for the wonderful birthday gift!"
