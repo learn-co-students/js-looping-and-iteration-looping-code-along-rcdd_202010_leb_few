e4f91197156a0416a26671b2b8434f2c727c9c13
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2020-11-17 16:19:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(name, event){
+  let message= [];
+  let mess= "";
+  console.log(name.length);
+  for(let i=0; i<name.length; i++){
+  mess=`Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+  message.push(mess);
+  
+}
+  return message;
+}
+let name =["Ada","Brendan","Ali"];
+writeCards(name,"birthday");
+
+function countDown(x){
+while(x>-1){
+  console.log(x);
+  x=x-1;
+}
+}
