74d4168580ad6b3328b8c1b87e6f34d063ec4423
bassam edelbi <bassamedelbi360@gmail.com>
2020-12-28 10:55:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  let x = []
+  for ( let i = 0; i < names.length; i++ ) {
+    x.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+  }
+  return x
+}
+function countDown(starting) {
+  while (starting>0) {
+    console.log(starting);
+    starting-=1;
+  }
+  console.log(starting);
+}
