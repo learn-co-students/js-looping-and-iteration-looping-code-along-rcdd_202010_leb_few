2a6a22804880a250f9a52bda4450b85465ed5226
Ibrahim Nour <ibrahamnour22@gmail.com>
2021-01-01 23:32:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(a,event){
+  let thanksArray=[];
+  for (let i in a){
+    thanksArray.push(`Thank you, ${a[i]}, for the wonderful ${event} gift!`)
+  }
+  return thanksArray;
+}
+
+function countDown(n){
+  for (let i=n; i>=0; i--){
+    console.log(i);
+  }
+}
